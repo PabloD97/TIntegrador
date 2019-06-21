2074ff8e9fe2dbd65cdc75a37d230662b3292dc9
Pablo <Pablo@DESKTOP-FKBJ0KB>
2019-06-21 13:02:47 -0300

    El packege prueboMetodos lo hice para probar cosas antes de usarlas en el tp

---
+++ src/prueboMetodos/Pibito.java
@@ -0,0 +1,17 @@
+package prueboMetodos;
+
+public class Pibito {
+
+	private Integer n;
+	
+	public Pibito(int i) {
+		this.n=i;
+	}
+	
+	public void setNumero(int n) {
+		this.n = n;
+	}
+	public Integer dameNumero() {
+		return this.n;
+	}
+}
