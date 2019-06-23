8ab96ccf76ad3453b9ef82a2688ab61a31c16c55
Pablo <Pablo@DESKTOP-FKBJ0KB>
2019-06-23 11:54:56 -0300

    Agregue el algoritmo para devolver las encuestas por orden alfabetico

--- src/encuestasDisponibles/OrdenadasPorProyecto.java
+++ src/encuestasDisponibles/OrdenadasPorProyecto.java
@@ -1,16 +1,28 @@
 package encuestasDisponibles;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import encuesta.Encuesta;
 import investigador.Investigador;
+import proyecto.Proyecto;
 
 public class OrdenadasPorProyecto implements IEstrategia {
 
+	
+	// agregado el algoritmo para dar las ncuestas ordenadas por orden alfabetico... espero que este bien
 	@Override
 	public List<Encuesta> dameLasEncuestas(Investigador investigador) {
-		// TODO Auto-generated method stub
-		return null;
+
+		List<Proyecto>lista1= investigador.getProyectos();
+		List<Encuesta> listaEncuestas= new ArrayList<Encuesta>();
+		
+		for(Proyecto proyecto : lista1) {
+			 for(Encuesta encuestas : proyecto.encuestasPorOrdenAlfabetico() ) {
+				 listaEncuestas.add(encuestas);
+			 }
+			}
+			return listaEncuestas;
 	}
 
 }
