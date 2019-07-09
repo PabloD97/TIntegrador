a924c96099014f4be708a9609cfc986df4a08059
Pablo <Pablo@DESKTOP-FKBJ0KB>
2019-07-08 21:30:27 -0300

    Finalizado el test de preguntaAbierta. Falta hacer el de preguntaDeSimpleSeleccion

--- test/preguntas/TestPreguntaAbierta.java
+++ test/preguntas/TestPreguntaAbierta.java
@@ -19,15 +19,14 @@ class TestPreguntaAbierta {
 	PreguntaAbierta preguntaAbierta2;
 	
 	Encuesta encuesta;
-	Encuestado contenedor;
+	Encuestado encuestado;
 	
 	@BeforeEach
 	void setUp() throws Exception {
 		
-		
+		preguntaAbierta2= new PreguntaAbierta(null, null);
 		encuesta= mock(Encuesta.class);
-		//preguntaAbierta2= new PreguntaAbierta
-		
+		encuestado= spy(new Encuestado(null, null, encuesta));
 		preguntaAbierta = new PreguntaAbierta("cual es tu nombre?", preguntaAbierta2);
 		
 	}
@@ -58,6 +57,26 @@ class TestPreguntaAbierta {
 	void testAQueEncuestaPerteneces() {
 		preguntaAbierta.setEncuesta(encuesta);
 		assertEquals(encuesta , preguntaAbierta.getEncuesta() );
+	}
+	
+	@Test 
+	void testSeEscribeLaRespuesta() {
+		preguntaAbierta.escribirRespuesta("pepito", encuestado);
+		assertTrue(encuestado.getRespuestasDelEncuestado().isEmpty());
+		assertEquals(new Integer(1), encuestado.getRespuestasElegidas().size());
+	}
+	
+	@Test
+	void testAlguienRespondeLaPregunta() {
+		encuestado.setPreguntaActual(preguntaAbierta);
+		preguntaAbierta.escribirRespuesta("pepito", encuestado);
+		encuestado.responder();
+		assertEquals( preguntaAbierta2.getAnteriorPregunta() , preguntaAbierta );
+		assertTrue(encuestado.getRespuestasElegidas().isEmpty());
+		assertTrue(!encuestado.getRespuestasDelEncuestado().isEmpty());
+		assertEquals(preguntaAbierta2, encuestado.getPreguntaActual());
+
+		
 	}
 }
 
